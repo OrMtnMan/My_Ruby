1041e1f8e1e81bbcdb36c067b6459c2253684dcb
OrMtnMan <ranma567001@yahoo.com>
2019-03-15 20:43:47 -0700

    update to TTT

--- RB101-109/Lesson_6/tic-tac-toe.rb
+++ RB101-109/Lesson_6/tic-tac-toe.rb
@@ -1,73 +1,79 @@
-ST_STR = 'Ready to play? [0] - Start, [1] - Exit'
-CPU_DIFF_STR = 'Select a difficulty: [0] - Easy, [1] - Hard'
-PLR_FST_STR = 'Who should go first? [0] - You, [1] - CPU,' \
+START_STR = 'Ready to play? [0] - Start, [1] - Exit'
+CPU_DIFFICULTY_STR = 'Select a difficulty: [0] - Easy, [1] - Hard'
+WHOS_FIRST_STR = 'Who should go first? [0] - You, [1] - CPU,' \
               ' [2] - Random'
 X_OR_O_STR = 'Do you want to be O or X? [0] - O, [1] - X'
 MAKE_MOVE = 'Select a box for your move or [0] to exit.'
+WIN_COMBOS = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+              [0, 3, 6], [1, 4, 7], [2, 5, 8],
+              [0, 4, 8], [2, 4, 6]]
+CORNERS = [0, 2, 6, 8]
+BOARD_SPLIT = '-------------'.center(60)
+BOARD = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-def prompt_ret(str, ary)
+def prompt_return(str, ary)
   inp = ''
   loop do
     puts str
-    inp = gets.chomp.to_i
-    break if ary.include?(inp)
+    inp = gets.chomp
+    break if ary.include?(inp.to_i) && inp == inp.to_i.to_s
     puts 'Please make a valid selection'
   end
-  inp
+  inp.to_i
 end
 
 def print_board(ary)
-  puts '-------------'.center(60)
-  puts "| #{ary[0]} | #{ary[1]} | #{ary[2]} |".center(60)
-  puts '-------------'.center(60)
-  puts "| #{ary[3]} | #{ary[4]} | #{ary[5]} |".center(60)
-  puts '-------------'.center(60)
-  puts "| #{ary[6]} | #{ary[7]} | #{ary[8]} |".center(60)
-  puts '-------------'.center(60)
+  puts BOARD_SPLIT
+  puts print_line(ary, 0)
+  puts BOARD_SPLIT
+  puts print_line(ary, 3)
+  puts BOARD_SPLIT
+  puts print_line(ary, 6)
+  puts BOARD_SPLIT
 end
 
-def play_game(turn, x_or_o, cpu_diff)
-  x_or_o_b = 0
-  x_or_o_b = 1 if x_or_o.zero?
-  board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  x_o_ary = ['O', 'X']
-  winner = ''
+def print_line(ary, line)
+  "| #{ary[line]} | #{ary[line + 1]} | #{ary[line + 2]} |".center(60)
+end
 
-  loop do
-    system 'cls'
+def player_turn?(turn)
+  turn.zero?
+end
+
+def play_game(turn, x_or_o, cpu_difficulty, winner = '', board = BOARD)
+  while winner == ''
+    system('clear') || system('cls')
     print_board(board)
     rem_moves = board.select { |s| s != 'X' && s != 'O' }
-    if turn.zero?
+    if player_turn?(turn)
       turn = 1
-      move = prompt_ret(MAKE_MOVE, rem_moves << 0)
-      winner = 0 if move.zero?
+      move = prompt_return(MAKE_MOVE, rem_moves << 0)
       break if move.zero?
-      board[move - 1] = x_o_ary[x_or_o]
+      board[move - 1] = x_or_o[:player]
     else
-      turn = 0
-      if cpu_diff.zero?
-        board[rem_moves.sample - 1] = x_o_ary[x_or_o_b]
-      else
-        board[hard_ai(board, x_or_o, x_or_o_b) - 1] = x_o_ary[x_or_o_b]
-      end
+      turn = cpu_turn(board, cpu_difficulty, x_or_o)
     end
-    winner = det_winner(board, x_or_o, x_or_o_b)
-    break unless winner == ''
+    winner = det_winner(board, x_or_o)
   end
 
   winner
 end
 
-def det_winner(board, plr, cpu)
-  win_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-                [0, 3, 6], [1, 4, 7], [2, 5, 8],
-                [0, 4, 8], [2, 4, 6]]
-  x_o_ary = ['O', 'X']
+def cpu_turn(board, cpu_difficulty, x_or_o)
+  if cpu_difficulty.zero?
+    board[rem_moves.sample - 1] = x_or_o[:cpu]
+  else
+    board[hard_ai(board, x_or_o) - 1] = x_or_o[:cpu]
+  end
+  0
+end
+
+def det_winner(board, x_or_o)
   winner = ''
-  win_combos.each do |combo|
-    if combo.map { |n| board[n] }.count(x_o_ary[plr]) == 3
+  WIN_COMBOS.each do |combo|
+    if combo.map { |n| board[n] }.count(x_or_o[:player]) == 3
       winner = 'You'
-    elsif combo.map { |n| board[n] }.count(x_o_ary[cpu]) == 3
+    elsif combo.map { |n| board[n] }.count(x_or_o[:cpu]) == 3
       winner = 'Computer'
     elsif (board.count('X') + board.count('O')) == 9
       winner = 'draw'
@@ -76,79 +82,93 @@ def det_winner(board, plr, cpu)
   winner
 end
 
-def hard_ai(board, plr, cpu)
-  win_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-                [0, 3, 6], [1, 4, 7], [2, 5, 8],
-                [0, 4, 8], [2, 4, 6]]
-  cnrs = [0, 2, 6, 8]
-  x_o_ary = ['O', 'X']
-  if (board.count('X') + board.count('O')).zero?
-    return 5
-  else
-    win_combos.each do |combo|
-      plr_in = combo.map { |n| board[n] }.count(x_o_ary[plr])
-      cpu_in = combo.map { |n| board[n] }.count(x_o_ary[cpu])
+def hard_ai(board, x_or_o)
+  corner_map = CORNERS.map { |n| board[n] }
 
-      if plr_in == 2 && cpu_in < 1
-        combo.map { |n| board[n] }.each { |n| return n if (1..9).cover?(n) }
-      elsif cpu_in == 2 && plr_in < 1
-        combo.map { |n| board[n] }.each { |n| return n if (1..9).cover?(n) }
-      end
+  return 5 if (board.count('X') + board.count('O')).zero?
+
+  return offense_defense(board, x_or_o) if !offense_defense(board, x_or_o).nil?
+
+  return corner_play(board, x_or_o) if corner_map.include?(1..9)
+
+  board.select { |s| s != 'X' && s != 'O' }.sample
+end
+
+def offense_defense(board, x_or_o)
+  WIN_COMBOS.each do |combo|
+    combo_map = combo.map { |n| board[n] }
+    plr_in = combo_map.count(x_or_o[:player])
+    cpu_in = combo_map.count(x_or_o[:cpu])
+
+    if (plr_in == 2 && cpu_in < 1) || (cpu_in == 2 && plr_in < 1)
+      combo_map.each { |n| return n if (1..9).cover?(n) }
     end
-    if cnrs.map { |n| board[n] }.include?(1..9)
-      cnrs.map { |n| board[n] }.select { |n| (1..9).cover?(n) }.each do |n|
-        win_combos.select { |a| a.include?(n - 1) }.each do |combo|
-          if combo.map { |nr| board[nr] }.include?(x_o_ary[plr]) == false
-            return false
-          end
-        end
+  end
+
+  nil
+end
+
+def corner_play(board, x_or_o)
+  corner_map = CORNERS.map { |n| board[n] }
+  corner_map_nrs = corner_map.select { |n| (1..9).cover?(n) }
+
+  corner_map_nrs.each do |number|
+    win_combo_with_nr = WIN_COMBOS.select { |a| a.include?(number - 1) }
+    win_combo_with_nr.each do |combo|
+      if combo.map { |nr| board[nr] }.include?(x_or_o[:player]) == false
+        number
       end
-    else
-      return board.select { |s| s != 'X' && s != 'O' }.sample
     end
   end
 end
 
 welcome_text = 'Welcome to Connor\'s Tic-Tac-Toe!'
 
-score_plr = 0
-score_cpu = 0
+score = { player: 0, cpu: 0, total: 0 }
 
 loop do
-  system 'cls'
+  system('clear') || system('cls')
 
   puts welcome_text.center(60, '-')
+  puts ''
+  puts "The score is: You - #{score[:player]}, " \
+       "CPU - #{score[:cpu]}".center(60)
+  puts ''
+  puts "First one to #{score[:total]} wins is the " \
+       "overall winner!".center(60)
+  puts ''
 
-  start = prompt_ret(ST_STR, [0, 1])
+  start = prompt_return(START_STR, [0, 1])
 
   break if start == 1
 
-  cpu_diff = prompt_ret(CPU_DIFF_STR, [0, 1])
-  plr_fst = prompt_ret(PLR_FST_STR, [0, 1, 2])
-  x_or_o = prompt_ret(X_OR_O_STR, [0, 1])
+  cpu_difficulty = prompt_return(CPU_DIFFICULTY_STR, [0, 1])
+  whos_first = prompt_return(WHOS_FIRST_STR, [0, 1, 2])
+  x_or_o = { player: '', cpu: '' }
+  x_or_o[:player] = prompt_return(X_OR_O_STR, [0, 1]).zero? ? 'O' : 'X'
+  x_or_o[:cpu] = x_or_o[:player] == 'O' ? 'X' : 'O'
 
-  turn = plr_fst == 2 ? [0, 1].sample : plr_fst
+  turn = whos_first == 2 ? [0, 1].sample : whos_first
 
-  winner = play_game(turn, x_or_o, cpu_diff)
+  winner = play_game(turn, x_or_o, cpu_difficulty)
 
-  break if winner == 0
+  break if winner == ''
 
-  winner == 'You' ? score_plr += 1 : score_cpu += 1
+  winner == 'You' ? score[:player] += 1 : score[:cpu] += 1
 
   if winner == 'draw'
-    welcome_text = "Draw! The score is  You - #{score_plr}" \
-                   ", CPU - #{score_cpu}. Play again?"
-  elsif score_plr == 5 || score_cpu == 5
-    welcome_text = if score_plr == 5
+    welcome_text = "Draw! Play again?"
+  elsif score[:player] == score[:total] || score[:cpu] == score[:total]
+    welcome_text = if score[:player] == score[:total]
                      'You win! Play again?'
                    else
-                     'CPU wins, play again?'
+                     'CPU wins... Play again?'
                    end
 
-    score_plr = 0
-    score_cpu = 0
+    score[:player] = 0
+    score[:cpu] = 0
   else
-    welcome_text = "#{winner} #{winner == 'You' ? 'win' : 'wins'}! The score" \
-                   " is You - #{score_plr}, CPU - #{score_cpu}. Play again?"
+    welcome_text = "#{winner} #{winner == 'You' ? 'win' : 'wins'}!" \
+                   "Play again?"
   end
 end
